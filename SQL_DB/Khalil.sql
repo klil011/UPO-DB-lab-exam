c04cc9bacb763aef0bc36d9f1d83bb29c2478487
Kairos011 <49352166+Kairos011@users.noreply.github.com>
2020-03-19 16:22:30 +0100

    modifica not null

--- SQL_DB/Khalil.sql
+++ SQL_DB/Khalil.sql
@@ -16,9 +16,9 @@ create table Struttura_corrente (
 create Risposte (
 	ID_risposta serial not null,
 	editore varchar(20),
-	data_pubblicazione timestamp,
-	testo varchar(500),
-	tipo_risposta boolean,
+	data_pubblicazione timestamp not null,
+	testo varchar(500) not null,
+	tipo_risposta boolean not null,
 	recensione integer not null,
 	constraint Risposte_PK primary key(ID_risposta),
 	constraint Recensione_FK_Risposte foreign key(recensione) references Recensione(ID_recensione),
@@ -30,10 +30,10 @@ create Prenotazione(
 	n_pren serial not null, 
 	struttura integer, 
 	cliente varchar(16), 
-	data_inizio data, 
-	data_fine data, 
-	check_in timestamp, 
-	check_out timestamp
+	data_inizio data not null, 
+	data_fine data not null, 
+	check_in timestamp not null, 
+	check_out timestamp not null,
 	constraint Prenotazione_PK primary key(n_pren),
 	constraint Struttura_FK_Prenotazione foreign key(struttura) references Struttura_corrente(ID_struttura) on delete set null,
 	constraint Cliente_FK_Prenotazione foreign key(cliente) references Utente(CF)
@@ -41,7 +41,7 @@ create Prenotazione(
 
 create Servizio(
 	ID_servizio serial not null, 
-	nome varchar(20), 
+	nome varchar(20) not null, 
 	descrizione varchar(20), 
 	icona varchar(200)
 );
@@ -60,12 +60,12 @@ create StanzaC(
 	struttura integer not null, 
 	internoA smallint not null, 
 	piano smallint not null, 
-	prezzo numeric check(prezzo > 0), 
-	disponibilitàSt boolean, 
+	prezzo numeric check(prezzo > 0) not null, 
+	disponibilitàSt boolean not null, 
 	descrizioneSt varchar(500), 
-	numero_posti_ letto smallint check(numero_posti_ letto > 0), 
-	numero_letti smallint check(numero_letti > 0), 
-	numero_bagni smallint check(bagni >= 0), 
+	numero_posti_ letto smallint check(numero_posti_ letto > 0) not null, 
+	numero_letti smallint check(numero_letti > 0) not null, 
+	numero_bagni smallint check(bagni >= 0) not null, 
 	tipo_letto varchar(20),
 	constraint StanzaC_PK primary key(InternoS, n_stanza, struttura, internoA, piano),
 	constraint Struttura_FK_StanzaC foreign key(struttura) references AppartamC(struttura) on delete cascade,
