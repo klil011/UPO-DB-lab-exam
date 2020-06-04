15577573a6b4e8e96607ef786951cefc8a91c07d
Kairos011 <49352166+Kairos011@users.noreply.github.com>
2020-06-04 13:01:11 +0200

    Update Monte_Mounir_Peretto_SQL.sql

--- SQL_MusicBrainz/Monte_Mounir_Peretto_SQL.sql
+++ SQL_MusicBrainz/Monte_Mounir_Peretto_SQL.sql
@@ -10,8 +10,10 @@ L'attributo language non presenta valori nulli ciò e verificabile con la query 
 SELECT COUNT(*) AS Numero_Lingue FROM release GROUP BY language;
 
 /*
-
 2)
+Elencare gli artisti che hanno cantato canzoni in italiano (il risultato deve contenere il nome dell’artista
+e il nome della lingua).
+*/
 Select artist.name, area.name
 from artist join artist_credit_name on artist.id = artist_credit_name.artist 
 			join artist_credit on artist.id = artist_credit.id
@@ -23,6 +25,23 @@ where language = (
 	from area
 	where name = 'Italy')
 
+<<<<<<< HEAD
+3)
+
+select name 
+from release 
+where language is null;
+
+=======
+/*
+3)
+Elencare le release di cui non si conosce la lingua (il risultato deve contenere soltanto il nome della
+release).
+*/
+select name from release where language is null;
+
+/*
+>>>>>>> d7d6c40ec8918389f9cb2dfc70b13111ec7314cf
 4)
 Elencare le release che nel nome hanno la parola “love”, in qualsiasi posizione (quindi anche in prima
 posizione; il risultato deve contenere soltanto il nome della release).
@@ -34,13 +53,43 @@ L'attributo name non presenta valori nulli
 SELECT name FROM release WHERE name ILIKE '%love%';
 
 /*
-
 5)
+Elencare tutti gli pseudonimi di Prince con il loro tipo, se disponibile (il risultato deve contenere lo
+pseudonimo dell'artista, il nome dell’artista (cioè Prince) e il tipo di pseudonimo (se disponibile)).
+*/
 Select artist.name, artist_alias.name, artist_alias.type
 from artist_alias full join artist on artist = artist.id
 where artist.name = 'Prince';
 
+<<<<<<< HEAD
+6)
 
+=======
+/*
+6)
+Elencare le release di gruppi inglesi ancora in attività (il risultato deve contenere il nome del gruppo e il
+nome della release e essere ordinato per nome del gruppo e nome della release)
+*/
+//nome gruppi interessati e codice id
+>>>>>>> d7d6c40ec8918389f9cb2dfc70b13111ec7314cf
+create view A as 
+select artist.name, artist_credit.id
+from artist
+inner join artist_credit on artist_credit.name = artist.name;
+<<<<<<< HEAD
+
+select A.name art_name, release.name rel_name
+from A
+inner join release on A.id = release.artist_credit;
+
+=======
+>>>>>>> d7d6c40ec8918389f9cb2dfc70b13111ec7314cf
+
+select A.name art_name, release.name rel_name
+from A
+inner join release on A.id = release.artist_credit;
+
+/*
 7)
 Trovare le release in cui il nome dell’artista è diverso dal nome accreditato nella release (il risultato deve
 contenere il nome della release, il nome dell’artista accreditato (cioè artist_credit.name) e il nome
@@ -59,8 +108,10 @@ JOIN (
 ) AS art_crdt_name_diff ON art_crdt_name_diff.art_crdt_id = release.artist_credit;
 
 /*
-
 8)
+Trovare gli artisti con meno di tre release (il risultato deve contenere il nome dell’artista ed il numero di
+release).
+*/
 Select artist.name, count(*)n_release
 from artist join artist_credit_name on artist.id = artist_credit_name.artist 
 			join artist_credit on artist.id = artist_credit.id
@@ -68,7 +119,74 @@ from artist join artist_credit_name on artist.id = artist_credit_name.artist
 group by artist.name
 having count (release.artist_credit) < 3
 
+<<<<<<< HEAD
 
+9)
+
+
+
+=======
+/*
+9)
+Trovare la registrazione più lunga di un’artista donna (il risultato deve contenere il nome della
+registrazione, la sua durata in minuti e il nome dell’artista; tenere conto che le durate sono memorizzate
+in millesimi di secondo) (scrivere due versioni della query con e senza operatore aggregato MAX).
+*/
+/*CON AGGREGATORE MAX*/
+
+/*Trovo tutte le artiste donne*/
+create view ART_FEM as
+select artist.id, artist.name
+from artist
+where gender = (select id from gender where name = 'Female');
+
+/*tutti gli artisti con relativo nome del recording*/
+create view ALL_ART_REC as
+select recording.name as name_rec, artist.name name_art, recording.length
+from recording
+inner join artist on recording.artist_credit = artist.id;
+
+/*nome artista donna con relativo nome recording e lunghezza*/
+create view ALL_ART_FEM_REC as
+select name_art, ALL_ART_REC.name_rec, ALL_ART_REC.length
+from ALL_ART_REC
+inner join ART_FEM on name_art = ART_FEM.name;
+
+create view FEM_REC_MAX as
+select max(length) l_max
+from ALL_ART_FEM_REC;
+
+select ALL_ART_FEM_REC.name_art, ALL_ART_FEM_REC.name_rec, ALL_ART_FEM_REC.length
+from ALL_ART_FEM_REC, FEM_REC_MAX
+where FEM_REC_MAX.l_max = ALL_ART_FEM_REC.length;
+
+/*SENZA AGGREGATORE MAX*/
+
+/*Trovo tutte le artiste donne*/
+create view ART_FEM as
+select artist.id, artist.name
+from artist
+where gender = (select id from gender where name = 'Female');
+
+/*tutti gli artisti con relativo nome del recording*/
+create view ALL_ART_REC as
+select recording.name as name_rec, artist.name name_art, recording.length
+from recording
+inner join artist on recording.artist_credit = artist.id;
+
+/*nome artista donna con relativo nome recording e lunghezza*/
+create view ALL_ART_FEM_REC as
+select name_art, ALL_ART_REC.name_rec, ALL_ART_REC.length
+from ALL_ART_REC
+inner join ART_FEM on name_art = ART_FEM.name
+where ALL_ART_REC.length is not null
+ORDER BY ALL_ART_REC.length DESC;
+
+select *
+from ALL_ART_FEM_REC
+limit 1;
+/*
+>>>>>>> d7d6c40ec8918389f9cb2dfc70b13111ec7314cf
 10)
 Elencare le lingue cui non corrisponde nessuna release (il risultato deve contenere il nome della lingua,
 il numero di release in quella lingua, cioè 0, e essere ordinato per lingua) (scrivere due versioni della
@@ -99,8 +217,11 @@ ORDER BY name;
 
 /*
 11)
-
-a)
+Ricavare la seconda registrazione per lunghezza di un artista uomo (il risultato deve comprendere l'artista
+accreditato, il nome della registrazione e la sua lunghezza) (scrivere due versioni della query; almeno
+una delle due versioni non deve utilizzare le viste).
+*/
+--a)
 
 Select recording.length, recording.artist_credit, recording.name
 from artist join artist_credit_name on artist.id = artist_credit_name.artist 
@@ -112,7 +233,7 @@ where gender = 1 and recording.length < ( select max(length)
 group by recording.length, recording.artist_credit, recording.name
 
 
-b)
+--b)
 
 create TEMP view  recLength as
 	select length, artist_credit, name
@@ -125,7 +246,52 @@ select recLength.length, recLength.artist_credit, recLength.name
 from artist join recLength on artist.id = recLength.artist_credit
 where gender = 1
 
+/*
+12)
+Per ogni stato esistente riportare la lunghezza totale delle registrazioni di artisti di quello stato (il risultato
+deve comprendere il nome dello stato e la lunghezza totale in minuti delle registrazioni (0 se lo stato non
+ha registrazioni) (scrivere due versioni della query; almeno una delle due versioni non deve utilizzare le
+viste).
+*/
 
+--query senza vista
+select tot_rec_length, area.name
+from
+(
+	select sum(rec_length) as tot_rec_length, area
+	from
+	(
+		select sum(COALESCE(recording.length, 0)) as rec_length, artist.area as area
+		from artist
+		join artist_credit_name on artist.id = artist_credit_name.artist
+		join artist_credit on artist_credit.id = artist_credit_name.artist_credit
+		right join recording on recording.artist_credit = artist_credit.id
+		group by artist.area
+	) as tot_area_rec
+	group by tot_area_rec.area
+) as tot_rec_for_area
+join area on area.id = tot_rec_for_area.area
+
+--query con vista
+CREATE TEMP VIEW rec_length_per_area AS
+select sum(COALESCE(recording.length, 0)) as rec_length, artist.area as area
+from artist
+join artist_credit_name on artist.id = artist_credit_name.artist
+join artist_credit on artist_credit.id = artist_credit_name.artist_credit
+right join recording on recording.artist_credit = artist_credit.id
+group by artist.area;
+
+select tot_rec_length, area.name
+from
+(
+	select sum(rec_length) as tot_rec_length, area
+	from rec_length_per_area
+	group by rec_length_per_area.area
+) as tot_rec_for_area
+join area on area.id = tot_rec_for_area.area;
+
+
+/*
 13)
 Ricavare gli artisti britannici che hanno pubblicato almeno 10 release (il risultato deve contenere il nome
 dell’artista, il nome dello stato (cioè United Kingdom) e il numero di release) (scrivere due versioni della
@@ -209,10 +375,11 @@ WHERE	num_rel_per_artist.artist = artist_state.artist_id AND
 		artist_state.nome_stato ILIKE 'united kingdom' AND
 		num_rel_per_artist.numero_di_release >= 10;
 
-
+/*
 14)
+*/
 
-a)
+--a)
 
 Select artist.name, count(release.artist_credit) as release_number
 from artist join artist_credit_name on artist.id = artist_credit_name.artist 
@@ -231,7 +398,7 @@ from (
 order by release_number desc
 
 
-b)
+--b)
 
 	create TEMP view  artistRelease as
 Select artist.name, count(release.artist_credit) as release_number
@@ -256,6 +423,33 @@ select name, release_number
 from artistRelease
 where release_number > (select * from avgRelease)
 order by release_number desc
+
+/*
+15)
+Ricavare il primo artista morto dopo Louis Armstrong (il risultato deve contenere il nome dell’artista, la
+sua data di nascita e la sua data di morte) (scrivere due versioni della query; almeno una delle due versioni
+non deve utilizzare le viste).
+*/
+/*CON VISTA*/
+
+create view art_ord_end as
+select name, begin_date_year, begin_date_month, begin_date_day, end_date_year, end_date_month, end_date_day
+from artist
+order by end_date_year, end_date_month, end_date_day;
+
+select name, begin_date_year, begin_date_month, begin_date_day, end_date_year, end_date_month, end_date_day
+from art_ord_end
+where end_date_year >= 1971 and end_date_month >= 7 and end_date_day > 6
+limit 1;
+
+/*SENZA VISTA*/
+
+select name, begin_date_year, begin_date_month, begin_date_day, end_date_year, end_date_month, end_date_day
+from (select name, begin_date_year, begin_date_month, begin_date_day, end_date_year, end_date_month, end_date_day
+from artist
+order by end_date_year, end_date_month, end_date_day) as foo
+where end_date_year >= 1971 and end_date_month >= 7 and end_date_day > 6
+limit 1;
 
 /*
 17)
